e68162df00899ca18d1b0d971c093618e51a6309
cool-breeze-bin <3195305550@qq.com>
2021-11-18 18:25:25 +0800

    2021.11.18天气实战最后提交

--- WeatherDemoApplication/app/src/main/java/com/fbb/weatherdemoapplication/MainActivity.java
+++ WeatherDemoApplication/app/src/main/java/com/fbb/weatherdemoapplication/MainActivity.java
@@ -2,13 +2,19 @@ package com.fbb.weatherdemoapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_choose_area);
+//        SQLiteDatabase db = LitePal.getDatabase();
+//        LitePal.getDatabase();
     }
 }
